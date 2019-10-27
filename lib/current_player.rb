347ff41b29f0414c135d07e5cb307c030fc1a17d
Winston Pemberton <winstonpemberton@gmail.com>
2019-10-27 17:42:23 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,10 @@
+board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
 def turn_count(board)
   counter = 0
   board.each do |index|
     if index == "X" || "O"
       counter += 1
+      puts counter
     end
   end
-return counter
 end
