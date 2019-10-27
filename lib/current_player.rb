ed84d2007c7afed90fe3fee8fd5b4aa9648aa807
Winston Pemberton <winstonpemberton@gmail.com>
2019-10-27 18:26:43 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,13 +6,3 @@ def turn_count(board)
       return
     end
   end
-
-  counter = board.size # Step 1
-apples_taken_out = 0 # Step 2
-
-# Step 3 + 4
-while apples_taken_out < apples_in_basket
-    puts "Taking out #{basket[apples_taken_out]}"
-    apples_taken_out += 1
-end
-end
